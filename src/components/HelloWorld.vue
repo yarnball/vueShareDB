dd3b012926e2b89b2ca6af35e5ff50d5d415732e
Yarnball <yconomos@gmail.com>
2018-05-12 16:47:12 +1000

    using simple counter as example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{ res }}
+    {{ JSON.stringify(res) }}
     <button @click='testShare()'> Test shareDB</button>
   </div>
 </template>
@@ -16,24 +16,25 @@ export default {
     }
   },
   mounted () {
-    var query = connection.createSubscribeQuery('players', {$sort: {score: -1}})
-    query.on('ready', update)
-    query.on('changed', update)
-    function update () {
-      console.log('magic', query.results)
-      this.res = query.results
-      // comp.setState({players: query.results});
-    }
-    console.log(this.$el) // I'm text inside the component.
+    var doc = connection.get('examples', 'counter')
+    doc.subscribe(this.showNumbers)
+    doc.on('op', this.showNumbers)
   },
   methods: {
+    showNumbers () {
+      var doc = connection.get('examples', 'counter')
+      this.res = doc.data.numClicks
+    },
     testShare () {
-      var op = [{p: ['score'], na: 15}]
-      connection.get('players', 1).submitOp(op, function (err) {
-        console.log('adding this?', connection.get('players', 1).data)
-        if (err) { console.error(err) }
-      })
-      this.res = connection.get('players', 1).data.score
+      var doc = connection.get('examples', 'counter')
+      doc.submitOp([{p: ['numClicks'], na: 1}])
+      // console.log( doc.data.numClicks)
+      // var op = [{p: ['score'], na: 15}]
+      // connection.get('players', 1).submitOp(op, function (err) {
+      //   console.log('adding this?', connection.get('players', 1).data)
+      //   if (err) { console.error(err) }
+      // })
+      // this.res = connection.get('players', 1).data.score
       // console.log('see', connection.get('players', 1).data )
     }
 
